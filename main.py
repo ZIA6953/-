c9eb71ef744880a3d26ce8c813421e3e59830554
ZIA6953 <136125748+ZIA6953@users.noreply.github.com>
2023-06-09 23:54:12 +0300

    .juhu

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
+basic.show_string("SHIHANA!")
+basic.show_icon(IconNames.HEART)
+basic.show_leds("""
+. # # # .
+# . . . .
+ . # . . .
+ . . # . .
+  # # # . .
+""")
 def on_button_pressed_a():
     basic.show_number(2006)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    basic.show_number(20)
+    basic.show_number(17)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-basic.show_string("SHIHANA!")
-basic.show_icon(IconNames.HEART)
-basic.show_leds("""
-    # . . . #
-        # . . . #
-        # # # # #
-        # . . . #
-        # . . . #
-""")
--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
+basic.showString("SHIHANA!")
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+. # # # .
+# . . . .
+ . # . . .
+ . . # . .
+  # # # . .
+`)
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showNumber(2006)
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    basic.showNumber(20)
+    basic.showNumber(17)
 })
-basic.showString("SHIHANA!")
-basic.showIcon(IconNames.Heart)
-basic.showLeds(`
-    # . . . #
-        # . . . #
-        # # # # #
-        # . . . #
-        # . . . #
-`)
